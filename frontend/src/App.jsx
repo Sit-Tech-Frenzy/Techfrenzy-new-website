ecdb168506f8079170f3aaaea880c2005e9f7a7f
Aniket Ghosh <ghoshaniket1000@gmail.com>
2024-04-21 13:01:40 +0530

    Crated a basic timeline component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,20 @@
-import './App.css'
+import { Timeline } from './components/Timeline'
+import './index.css'
 
 function App() {
 
   return (
-    <>
-
-    </>
+    <div>
+      <div className='flex justify-center py-5 border-b'>
+        this is a app bar
+      </div>
+    <div className='pt-2'>
+      <Timeline />
+    </div>
+    <div className='pt-2'>
+      <Timeline />
+    </div>
+    </div>
   )
 }
 
---
+++ frontend/src/components/Prizes.jsx
@@ -0,0 +1,5 @@
+export const Prizes = () => {
+    return <div>
+        
+    </div>
+}
