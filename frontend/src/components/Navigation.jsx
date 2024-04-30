638d12dc39ddebdceede7f85fb59d28039eb45e8
Dhiraj Agarwal <abhinaygiri08@gmail.com>
2024-04-30 22:22:51 +0530

    navbar edits

--- frontend/src/components/Navigation.jsx
+++ frontend/src/components/Navigation.jsx
@@ -1,67 +1,36 @@
-import React, { useState } from "react";
-import { motion } from "framer-motion";
-import { IoHomeOutline, IoPersonOutline, IoAlarmOutline, IoPeopleOutline, IoChatbubbleOutline, IoSettingsOutline } from "react-icons/io5";
+import React, { useState } from 'react';
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 
-const Navigation = () => {
-  const Menus = [
-    { name: "Home", icon: <IoHomeOutline />, dis: "translate-x-0" },
-    { name: "About us", icon: <IoPersonOutline />, dis: "translate-x-16" },
-    { name: "Timeline", icon: <IoAlarmOutline />, dis: "translate-x-32" },
-    { name: "Members", icon: <IoPeopleOutline />, dis: "translate-x-48" },
-    { name: "Contact Us", icon: <IoChatbubbleOutline />, dis: "translate-x-64" },
-    { name: "Settings", icon: <IoSettingsOutline />, dis: "translate-x-80" },
-  ];
-  const [active, setActive] = useState(0);
+const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
 
   return (
-    <div className="h-full flex flex-col justify-center items-center">
-      <motion.div
-        className="rounded-xl shadow-[24px 24px 46px #aaaaaa, -24px -24px 46px #ffffff] bg-[#00df9a] max-h-[4.4rem] px-6 rounded-t-xl mt-4 "
-        whileHover={{ y: -5, transition: { type: "spring", stiffness: 300 } }}
-      >
-        <ul className="flex relative">
-          <span
-            className={`bg-rose-600 duration-500 ${Menus[active].dis} border-4 border-gray-900 h-16 w-16 absolute
-           -top-5 rounded-full`}
-          >
-            <span
-              className="w-3.5 h-3.5 bg-transparent absolute top-4 -left-[18px] 
-            rounded-tr-[11px] shadow-myShadow1"
-            ></span>
-            <span
-              className="w-3.5 h-3.5 bg-transparent absolute top-4 -right-[18px] 
-            rounded-tl-[11px] shadow-myShadow2"
-            ></span>
-          </span>
-          {Menus.map((menu, i) => (
-            <li key={i} className="w-16">
-              <a
-                className="flex flex-col text-center pt-6"
-                onClick={() => setActive(i)}
-              >
-                <span
-                  className={`text-xl cursor-pointer duration-500 ${
-                    i === active && "-mt-6 text-white"
-                  }`}
-                >
-                  {menu.icon}
-                </span>
-                <span
-                  className={` ${
-                    active === i
-                      ? "translate-y-4 duration-700 opacity-100"
-                      : "opacity-0 translate-y-10"
-                  } /*  */`}
-                >
-                  {menu.name}
-                </span>
-              </a>
-            </li>
-          ))}
-        </ul>
-      </motion.div>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white '>
+      <h1 className='w-full text-3xl font-bold text-[#00df9a]'>TechFrenzy</h1>
+      <ul className='hidden md:flex'>
+        <li className='p-4'>Home</li>
+        <li className='p-4'>Members</li>
+        <li className='p-4'>Timeline</li>
+        <li className='p-4'>About</li>
+        <li className='p-4'>Contact</li>
+      </ul>
+      <div onClick={handleNav} className='block md:hidden'>
+          {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
+      </div>
+      <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-gray-500 bg-gray-700 ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%] '}>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>TechFrenzy</h1>
+          <li className='p-4 border-b border-gray-600'>Home</li>
+          <li className='p-4 border-b border-gray-600'>Timeline</li>
+          <li className='p-4 border-b border-gray-600'>Members</li>
+          <li className='p-4 border-b border-gray-600'>About</li>
+          <li className='p-4'>Contact</li>
+      </ul>
     </div>
   );
 };
 
-export default Navigation;
+export default Navbar;
