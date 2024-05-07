9178cb4c2ee009b1cf7087384810a54df746f373
Aniket Ghosh <ghoshaniket1000@gmail.com>
2024-05-08 01:29:38 +0530

    minor style changes

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,7 +2,7 @@ import { FaTwitter, FaLinkedinIn } from "react-icons/fa";
 
 const Card = ({ id, name, image, position, company, xLink, linkedInLink }) => {
     return (
-        <div className="bg-gradient-to-br from-gray-900 to-red-700 rounded-xl shadow-md hover:shadow-xl transform hover:scale-105 transition-transform duration-200 ease-in-out cursor-pointer p-2 md:p-4 lg:m-2 mx-4 my-2">
+        <div className="bg-gradient-to-br from-gray-900 to-red-700 rounded-xl shadow-md hover:shadow-xl transform hover:scale-105 transition-transform duration-200 ease-in-out p-2 md:p-4 lg:m-2 mx-4 my-2">
             <div className="flex flex-col items-center mb-4">
                 <img src={image} className=" h-64 object-cover rounded-lg mb-4" alt={name} />
 
@@ -17,7 +17,7 @@ const Card = ({ id, name, image, position, company, xLink, linkedInLink }) => {
                     <FaTwitter className="text-white text-3xl" />
                 </a> */}
                 <a href={linkedInLink} target="_blank" rel="noopener noreferrer">
-                    <FaLinkedinIn className="text-white text-3xl" />
+                    <FaLinkedinIn className="text-white text-3xl cursor-pointer" />
                 </a>
             </div>
         </div>
--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -32,9 +32,6 @@ const Hero = () => {
   return (
     <div className='text-white'>
       <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-        <p className='text-[#00df9a] font-bold p-2'>
-          <AnimatedText text='LEARN GROW AND EXPLORE' />
-        </p>
         <h1 className='md:text-7xl sm:text-6xl text-6xl font-bold md:py-6 '>
           <AnimatedText text='TechFrenzy' />
         </h1>
