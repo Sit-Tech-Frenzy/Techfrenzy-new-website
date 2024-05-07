ef3956547faf1f522ac8ed526c106950dd3aa756
Aniket Ghosh <ghoshaniket1000@gmail.com>
2024-05-07 12:32:39 +0530

    Changed heading div line colors to green

--- frontend/src/components/About.jsx
+++ frontend/src/components/About.jsx
@@ -27,14 +27,14 @@ const About = () => {
       animate={controls}
       className='flex justify-center items-center h-screen'
     >
-      <div className='rounded-xl shadow-2xl p-8 sm:p-12 md:p-10 lg:p-20 mx-40 sm:mx-7 bg-gradient-to-tl from-opacity-10 to-transparent backdrop-blur-10 border border-opacity-18'>
-        <h3 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold text-center mb-4 text-[#00df9a]'>About Us</h3>
-        <hr className='bg-yellow-500 h-1 mt-6 mb-5' />
+      <div className='rounded-xl shadow-2xl p-8 sm:p-12 md:p-10 lg:p-20 mx-4 sm:mx-2 bg-gradient-to-tl from-opacity-10 to-transparent backdrop-blur-10 border border-opacity-18'>
+        <h3 className='text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold text-center mb-4 text-white'>About Us</h3>
+        <hr className='bg-[#00df9a] h-1 mt-6 mb-5' />
         <motion.p
           initial={{ x: '-100%' }}
           animate={{ x: 0 }}
           transition={{ duration: 0.5, delay: 0.3 }}
-          className='text-lg sm:text-xl md:text-2xl lg:text-3xl text-gray-700 pt-5'
+          className='text-lg sm:text-xl md:text-2xl lg:text-3xl text-gray-400 pt-5'
         >
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac felis sit amet augue venenatis fermentum. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam vel quo quaerat doloremque nemo libero exercitationem nesciunt distinctio suscipit dolor. Cumque vitae quae aspernatur enim voluptatibus nemo doloremque voluptas atque!Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem aliquid eaque explicabo harum id corrupti magnam error doloribus laudantium architecto sunt, totam quia cum nobis accusantium facere velit provident quibusdam.
         </motion.p>
--- frontend/src/components/BackgroundLabel.jsx
+++ frontend/src/components/BackgroundLabel.jsx
@@ -3,9 +3,9 @@ export const BackgroundLabel = ({ label, children }) => {
         <div>
             <div className="flex justify-center items-center h-full lg:py-36 py-28">
                 <div className="text-white lg:text-9xl text-7xl font-bold">
-                    <div className="bg-orange-800 h-1 w-full flex justify-center rounded" />
+                    <div className="bg-[#00df9a] h-1 w-full flex justify-center rounded" />
                     {children}
-                    <div className="bg-orange-800 h-1 w-34 flex justify-center rounded" />
+                    <div className="bg-[#00df9a] h-1 w-34 flex justify-center rounded" />
                 </div>
             </div>
         </div>
